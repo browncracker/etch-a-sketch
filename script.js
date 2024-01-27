6dfc3b1cbad1978e9618f7a86498e0379e68e836
browncracker <nealiumkansara@gmail.com>
2024-01-26 23:25:06 -0500

    added function to clear grid, need to figure out how to readd square event listener

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+//set vars for default items
 let container = document.querySelector('#container');
 let gridBtn = document.querySelector('#gridSelector');
+//generate default grid
 generateGrid(16);
 
+//set event listener for all squares
 let squares = document.querySelectorAll('.square');
-
 squares.forEach((square) => {
     square.addEventListener('mouseover', () => {
         square.setAttribute('style', 'background: black;');
     });
 });
 
+//set event listener for button
+gridBtn.addEventListener('click', () => {
+    let newSize = prompt('Enter grid size from 1-100 squares');
+    if (newSize>=1 && newSize <=100) {
+        clearGrid();
+        generateGrid(newSize);
+    } else {
+        prompt('Enter grid size from 1-100 squares');
+    }
+});
+
+//function to generate new grid of size gridSize x gridSize
 function generateGrid(gridSize) {
     for (i = 0; i < gridSize; i++) {
         let row = document.createElement('div');
@@ -22,4 +36,9 @@ function generateGrid(gridSize) {
         container.appendChild(row);
     }
     console.log('run');
+}
+
+//function to clear grid
+function clearGrid() {
+    container.innerHTML = '';
 }
